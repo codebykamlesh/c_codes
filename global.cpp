48b951eed68cf7fbdcc00b0d084d66361c94369e
Kamlesh Singh <purohitkamleshsingh@gmail.com>
2026-03-11 12:14:51 +0530

    Delete global.cpp

--- global.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int a = 10; // Global Initialized Variable 
-static int b = 20; // Static Initialized Variable
-
-int main(){
-    cout << a;
-    cout << b;
-}
